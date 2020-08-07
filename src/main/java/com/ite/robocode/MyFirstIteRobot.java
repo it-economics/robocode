d608c8febab45381a4c1b723bed0c387bef64c3d
Alex Kucksdorf <akucksdorf@it-economics.de>
2020-08-07 15:24:47 +0200

    chore: Add gradle setup (#2)

--- src/main/java/com/ite/robocode/MyFirstIteRobot.java
+++ src/main/java/com/ite/robocode/MyFirstIteRobot.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * https://robocode.sourceforge.io/license/epl-v10.html
  */
-package sample;
+package main.java.com.ite.robocode;
 
 
 import robocode.HitByBulletEvent;
