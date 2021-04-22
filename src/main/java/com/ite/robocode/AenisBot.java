b3fdf36789701853fe02f06a60f30f225edd5428
Aenis Chebil <aenis.chebil@alumni.fh-aachen.de>
2021-04-22 20:16:11 +0200

    runde 3

--- src/main/java/com/ite/robocode/AenisBot.java
+++ src/main/java/com/ite/robocode/AenisBot.java
@@ -11,17 +11,10 @@ import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 
 public class AenisBot extends Robot{
-<<<<<<< HEAD
     int dist = 20; // distance to move when we're hit
     public void run() {
         // Set colors
         setBodyColor(Color.green);
-=======
-    int dist = 400; // distance to move when we're hit
-    public void run() {
-        // Set colors
-        setBodyColor(Color.black);
->>>>>>> a631623f668a6861556a48ae65843e33281bb9a2
         setGunColor(Color.black);
         setRadarColor(Color.black);
 
@@ -31,7 +24,10 @@ public class AenisBot extends Robot{
         // Spin the gun around slowly... forever
         while (true) {
             turnGunRight(10);
-
+            if(getOthers() > 2) {
+                turnRight(40);
+                ahead(30);
+            }
         }
     }
 
@@ -40,21 +36,23 @@ public class AenisBot extends Robot{
      */
     public void onScannedRobot(ScannedRobotEvent e) {
         // If the other robot is close by, and we have plenty of life,
-        // fire hard!
-        if (e.getDistance() < 100 && getEnergy() > 50) {
-<<<<<<< HEAD
+        // fire hard
+        if (e.getDistance() < 50 && getEnergy() > 51) {
+            fire(50);
+        } // otherwise, fire 1.
+        else if (e.getDistance() < 100 && getEnergy() > 31) {
             fire(30);
         } // otherwise, fire 1.
         else {
-            fire(10);
-            ahead(400);
-            turnRight(40);
-=======
-            fire(25);
-        } // otherwise, fire 1.
-        else {
-            fire(10);
->>>>>>> a631623f668a6861556a48ae65843e33281bb9a2
+            if(getOthers() > 2) {
+                fire(40);
+            }
+            else
+            {
+                fire(10);
+                turnRight(e.getBearing());
+                ahead(e.getDistance());
+            }
         }
         // Call scan again, before we turn the gun
         scan();
@@ -64,11 +62,6 @@ public class AenisBot extends Robot{
      * onHitByBullet:  Turn perpendicular to the bullet, and move a bit.
      */
     public void onHitByBullet(HitByBulletEvent e) {
-<<<<<<< HEAD
-        turnRight(normalRelativeAngleDegrees((90 - getHeading() - e.getHeading())));
-=======
-        turnRight(normalRelativeAngleDegrees(90 - (getHeading() - e.getHeading())));
->>>>>>> a631623f668a6861556a48ae65843e33281bb9a2
 
         ahead(dist);
         dist *= -1;
