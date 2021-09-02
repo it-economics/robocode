26968c5d3d7223ac026c42b98eafa309885b6a89
Diana Barth <dianabarth87@googlemail.com>
2021-09-02 19:09:56 +0200

    Diana bot

---
+++ src/main/java/com/ite/robocode/DianaRobot.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2001-2019 Mathew A. Nelson and Robocode contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://robocode.sourceforge.io/license/epl-v10.html
+ */
+package com.ite.robocode;
+
+import robocode.Robot;
+
+public class DianaRobot extends Robot {
+
+    /**
+     * run:  Fire's main run function
+     */
+    public void run() {
+        fire(10);
+        while (true) {
+            turnRight(5);
+            scan();
+            fire();
+        }
+    }
+
+    /**
+     * onScannedRobot:
+     */
+    public void onScannedRobot(robocode.ScannedRobotEvent e) {
+        fire(100);
+        scan();
+    }
+
+    /**
+     * onHitByBullet:
+     */
+    public void onHitByBullet(robocode.HitByBulletEvent e) {
+        turnleft(45);
+        ahead(10);
+
+    }
+
+    /**
+     * onHitRobot:
+     */
+    public void onHitRobot(robocode.HitRobotEvent e) {
+        fire(10);
+        turnRight(45);
+        ahead(5);
+    }
+}
