5924b6e14d7e1f52d34e9f0bb75e3b46362f0ee7
Jannik <jaess105@uni-duesseldorf.de>
2021-09-02 18:58:01 +0200

    mit daten jetzt

--- src/main/java/com/ite/robocode/ExampleRobot.java
+++ src/main/java/com/ite/robocode/ExampleRobot.java
@@ -1,8 +1,7 @@
 /**
- * Copyright (c) 2001-2019 Mathew A. Nelson and Robocode contributors
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
+ * Copyright (c) 2001-2019 Mathew A. Nelson and Robocode contributors All rights reserved. This
+ * program and the accompanying materials are made available under the terms of the Eclipse Public
+ * License v1.0 which accompanies this distribution, and is available at
  * https://robocode.sourceforge.io/license/epl-v10.html
  */
 package com.ite.robocode;
@@ -11,38 +10,38 @@ import robocode.Robot;
 
 public class ExampleRobot extends Robot {
 
-    /**
-     * run:  Fire's main run function
-     */
-    public void run() {
-        fire(1);
-        while (true) {
-            ahead(5);
-            turnRight(5);
-        }
+  /**
+   * run:  Fire's main run function
+   */
+  public void run() {
+    fire(1);
+    while (true) {
+      ahead(5);
+      turnRight(5);
     }
+  }
 
-    /**
-     * onScannedRobot:
-     */
-    public void onScannedRobot(robocode.ScannedRobotEvent e) {
+  /**
+   * onScannedRobot:
+   */
+  public void onScannedRobot(robocode.ScannedRobotEvent e) {
 
-        scan();
-    }
+    scan();
+  }
 
-    /**
-     * onHitByBullet:
-     */
-    public void onHitByBullet(robocode.HitByBulletEvent e) {
+  /**
+   * onHitByBullet:
+   */
+  public void onHitByBullet(robocode.HitByBulletEvent e) {
 
-        scan();
-    }
+    scan();
+  }
 
-    /**
-     * onHitRobot:
-     */
-    public void onHitRobot(robocode.HitRobotEvent e) {
+  /**
+   * onHitRobot:
+   */
+  public void onHitRobot(robocode.HitRobotEvent e) {
 
-        scan();
-    }
+    scan();
+  }
 }
--- src/main/java/com/ite/robocode/first/UnserRobi.java
+++ src/main/java/com/ite/robocode/first/UnserRobi.java
@@ -1,2 +1,44 @@
-package com.ite.robocode.first;public class UnserRobi {
+package com.ite.robocode.first;
+
+import java.awt.AWTException;
+import java.awt.GraphicsDevice;
+
+
+import robocode.Robot;
+
+public class UnserRobi extends Robot {
+
+  /**
+   * run:  Fire's main run function
+   */
+  public void run() {
+    while (true) {
+      fire(1);
+      ahead(5);
+      turnRight(5);
+    }
+  }
+
+  /**
+   * onScannedRobot:
+   */
+  public void onScannedRobot(robocode.ScannedRobotEvent e) {
+    scan();
+  }
+
+  /**
+   * onHitByBullet:
+   */
+  public void onHitByBullet(robocode.HitByBulletEvent e) {
+
+    scan();
+  }
+
+  /**
+   * onHitRobot:
+   */
+  public void onHitRobot(robocode.HitRobotEvent e) {
+
+    scan();
+  }
 }
