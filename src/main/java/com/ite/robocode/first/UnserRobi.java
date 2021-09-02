2f0cf41e489dbc764ada8caf948039e716888770
Jannik <jaess105@uni-duesseldorf.de>
2021-09-02 20:31:20 +0200

    3rd improvements

--- src/main/java/com/ite/robocode/first/UnserRobi.java
+++ src/main/java/com/ite/robocode/first/UnserRobi.java
@@ -1,6 +1,7 @@
 package com.ite.robocode.first;
 
 import java.awt.AWTException;
+import java.awt.Color;
 import java.awt.GraphicsDevice;
 
 
@@ -13,11 +14,15 @@ public class UnserRobi extends Robot {
    * run:  Fire's main run function
    */
   public void run() {
+    setBodyColor(Color.getHSBColor(300, 100, 100));
+    setGunColor(Color.getHSBColor(300, 100, 100));
+    setRadarColor(Color.getHSBColor(300, 100, 100));
+    setBulletColor(Color.getHSBColor(300, 100, 100));
     drive();
-    turnGunRight(90);
     while (true) {
+      turnGunRight(getHeading() - getGunHeading() + 90);
       drive();
-      fire(1);
+      fire(5);
     }
   }
 
@@ -31,7 +36,7 @@ public class UnserRobi extends Robot {
   }
 
   public void onScannedRobot(robocode.ScannedRobotEvent e) {
-    fire(1);
+    fire(10);
   }
 
   /**
@@ -45,6 +50,8 @@ public class UnserRobi extends Robot {
    * onHitRobot:
    */
   public void onHitRobot(robocode.HitRobotEvent e) {
-    scan();
+    turnGunRight(getHeading() - getGunHeading());
+    final double power = getEnergy() - 15;
+    fireBullet(power > 1 ? power : getEnergy() - 1);
   }
 }
