12f7b763839428f5c125352781b021494ff1d207
Ferhat <36580753+Wayne42@users.noreply.github.com>
2021-11-05 15:55:08 +0100

    v4

--- src/main/java/com/ite/robocode/ClapTrap.java
+++ src/main/java/com/ite/robocode/ClapTrap.java
@@ -10,7 +10,7 @@ package com.ite.robocode;
 import robocode.*;
 import robocode.Robot;
 
-// import java.util.Random;
+import java.util.Random;
 
 import java.awt.*;
 
@@ -23,16 +23,16 @@ public class ClapTrap extends Robot {
 	 */
 
 	private boolean test = false;
+	private int runMode = 0;
+	private Random rand = new Random();
 	@Override
 	public void run() {
 		// Set colors
-		if(test){
+		if (test){
 			setBodyColor(Color.yellow);
 			setGunColor(Color.black);
 			setRadarColor(Color.red);
-			test = !test;
-		}else{
-			/*Random rand = new Random();
+		} else{
 			// Java 'Color' class takes 3 floats, from 0 to 1.
 			float r = rand.nextFloat();
 			float g = rand.nextFloat();
@@ -42,20 +42,24 @@ public class ClapTrap extends Robot {
 
 			setBodyColor(randomColor);
 			setGunColor(randomColor);
-			setRadarColor(randomColor); */
-
-			setBodyColor(Color.black);
-			setGunColor(Color.yellow);
-			setRadarColor(Color.red);
+			setRadarColor(randomColor);
 		}
+		test = !test;
 
 		// This will be the default behaviour of your robot
 		while(true){
-			ahead(100); //Go ahead 100 pixels
-			turnGunRight(360); //scan
-			back(75); //Go back 75 pixels
-			turnGunRight(360); //scan
-
+			if(runMode % 3 == 0){
+				ahead(100); //Go ahead 100 pixels
+				turnGunLeft(360); //scan
+				back(90); //Go back 75 pixels
+				turnGunLeft(360); //scan
+			}else{
+				ahead(100); //Go ahead 100 pixels
+				turnGunRight(360); //scan
+				back(75); //Go back 75 pixels
+				turnGunRight(360); //scan
+			}
+			runMode++;
 			//For each second the robot goes ahead 25 pixels.
 		}
 	}
@@ -92,31 +96,32 @@ public class ClapTrap extends Robot {
 	/**
 	 * onHitByBullet:  Ouch...our robot was hit by a bullet
 	 */
-	@Override
-	public void onHitByBullet(HitByBulletEvent e) {
-		turnGunLeft(10);
-		ahead(10);
-		scan();
-	}
 
-	/* coward mode */
-/*
-	double energy = getEnergy();
+	private int onHitMode = 0;
+	@Override
 	public void onHitByBullet(HitByBulletEvent e){
-		double bearing = e.getBearing(); //Get the direction which is arrived the bullet.
-		if(energy < 100){ // if the energy is low, the robot go away from the enemy
-			turnRight(-bearing); //This isn't accurate but release your robot.
-			ahead(100); //The robot goes away from the enemy.
+		double energy = getEnergy();
+		onHitMode++;
+		if(onHitMode % 2 == 0){
+			ahead(100);
+			scan();
+		}else{
+			double bearing = e.getBearing(); //Get the direction which is arrived the bullet.
+			if(energy < 100){ // if the energy is low, the robot go away from the enemy
+				turnRight(-bearing); //This isn't accurate but release your robot.
+				ahead(100); //The robot goes away from the enemy.
+			}
+			else
+				turnRight(360); // scan
 		}
-		else
-			turnRight(360); // scan
-	}*/
+	}
 
 	@Override
 	public void onHitWall(HitWallEvent e){
 		double bearing = e.getBearing(); //get the bearing of the wall
 		turnRight(-bearing); //This isn't accurate but release your robot.
 		ahead(100); //The robot goes away from the wall.
+		// scan();
 	}
 
 	/**
