c719a37b18a07f3a65e74255b65e7b74e4a2c29f
Ferhat <36580753+Wayne42@users.noreply.github.com>
2021-11-05 15:07:16 +0100

    claptrap v1 test

--- src/main/java/com/ite/robocode/ClapTrap.java
+++ src/main/java/com/ite/robocode/ClapTrap.java
@@ -12,6 +12,8 @@ import robocode.HitRobotEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
 
+// import java.util.Random;
+
 import java.awt.*;
 
 // IMPORTANT!!!
@@ -21,19 +23,49 @@ public class ClapTrap extends Robot {
 	/**
 	 * run:  Fire's main run function
 	 */
+
+	private boolean test = false;
 	@Override
 	public void run() {
 		// Set colors
-		setBodyColor(Color.black);
-		setGunColor(Color.black);
-		setRadarColor(Color.black);
+		if(test){
+			setBodyColor(Color.yellow);
+			setGunColor(Color.black);
+			setRadarColor(Color.red);
+			test = !test;
+		}else{
+			/*Random rand = new Random();
+			// Java 'Color' class takes 3 floats, from 0 to 1.
+			float r = rand.nextFloat();
+			float g = rand.nextFloat();
+			float b = rand.nextFloat();
+
+			Color randomColor = new Color(r, g, b);
+
+			setBodyColor(randomColor);
+			setGunColor(randomColor);
+			setRadarColor(randomColor); */
+
+			setBodyColor(Color.black);
+			setGunColor(Color.yellow);
+			setRadarColor(Color.red);
+		}
 
 		// This will be the default behaviour of your robot
 		while (true) {
-			turnGunRight(10);
-			ahead(20);
-			turnLeft(15);
+			betterTurnGunRight(10);
+			ahead(1);
 		}
+	}
+
+	public void betterTurnGunRight (double degree){
+		turnGunRight(degree);
+		turnRadarLeft(degree);
+	}
+
+	public void betterTurnGunLeft (double degree){
+		turnGunLeft(degree);
+		turnRadarRight(degree);
 	}
 
 	/**
@@ -41,7 +73,8 @@ public class ClapTrap extends Robot {
 	 */
 	@Override
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// do something here
+		// do something
+		fire(1);
 	}
 
 	/**
@@ -49,9 +82,15 @@ public class ClapTrap extends Robot {
 	 */
 	@Override
 	public void onHitByBullet(HitByBulletEvent e) {
-		// let's back off
-		back(10);
+		betterTurnGunLeft(10);
+		ahead(10);
 	}
+	/*
+	@Override
+	public void onHitWall(){
+		turnRadarLeft(90);
+		ahead(40);
+	}*/
 
 	/**
 	 * onHitRobot:  Yes, we hit a robot
